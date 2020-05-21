2822006bdc7a81e207d6d8d7c932f2d30a35bc8f
Lakshman Muthiah Venkatachalam <lakshmuthvenkat123@gmail.com>
2020-05-21 20:10:15 +0530

    upd
    upd

---
+++ Project Euler 3 Largest prime factor.py
@@ -0,0 +1,22 @@
+#https://www.hackerrank.com/contests/projecteuler/challenges/euler003/problem?isFullScreen=false
+
+import sys
+import math 
+
+def primeFactors(n): 
+    s=[]
+    while n % 2 == 0: 
+        s.append(2)
+        n = n / 2
+    for i in range(3,int(math.sqrt(n))+1,2): 
+        while n % i== 0: 
+            s.append(int(i))
+            n = n / i 
+    if n > 2: 
+        s.append(int(n))
+    return max(s)
+
+t = int(input().strip())
+for a0 in range(t):
+    n = int(input().strip())
+    print(primeFactors(n))
